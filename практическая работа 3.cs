da680adbe28e7fcc94f88374872daa95e9f1c418
Checha07 <58676214+Checha07@users.noreply.github.com>
2019-12-23 21:07:43 +0700

    Add files via upload

---
+++ практическая работа 3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _333
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double e = Convert.ToDouble(Console.ReadLine());
+            double b = 1;
+            double s = 0;
+            int i = 0;
+            while (b >= e)
+            {
+                i++;
+                b = (1.0 / (i * i));
+                s += b;
+            }
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
