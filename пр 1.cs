faf0e396d0e12b245bde0c7a7ad22306406602c6
Checha07 <58676214+Checha07@users.noreply.github.com>
2019-12-23 21:07:07 +0700

    Add files via upload

---
+++ пр 1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение х");
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y;
+
+            if (x < 1)
+            {
+                y = 1 / x;
+            }
+            else if (x > 2)
+            {
+                y = 4;
+            }
+            else
+            {
+                y = x * x;
+            }
+
+            Console.WriteLine("f(x) = "+y);
+
+        }
+    }
+}
