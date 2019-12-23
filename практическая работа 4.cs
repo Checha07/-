9fb2946782b3e7dc8e8b8dc63871a69effd85210
Checha07 <58676214+Checha07@users.noreply.github.com>
2019-12-23 21:08:27 +0700

    Add files via upload

---
+++ практическая работа 4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace практическая_работа_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int f = 10; f <= 99; f++)
+            {
+                bool flag = true;
+                for (int i = 2; i <= 9; i++)
+                {
+                    int s = (f / 10) + (f % 10);
+                    int s2 = (f * i / 100) + (f * i % 10) + (f * i % 100 / 10);
+                    if (s != s2)
+                    {
+                        flag = false;
+                    }
+                }
+                if (flag)
+                    Console.WriteLine(f + ",");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ практическая работа 5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace практическая_работа_5
+{
+    class Program
+    {
+        private static bool flag;
+
+        static void Main(string[] args)
+        {
+            int k = Convert.ToInt16(Console.ReadLine());
+            int d = 0;
+            for (int i=1; i<=k/2; i++)
+            {
+                if (k % i == 0)
+                {
+                    d += i;
+                }
+            }
+            if(d==k)
+            {
+                 flag = true;
+            }
+            else
+            {
+                 flag = false;
+            }
+            if(flag==true)
+            {
+                Console.WriteLine(d+"  Совершенное число");
+            }
+            else
+            {
+                Console.WriteLine("Несовершенное число");
+            }
+            Console.ReadLine();
+        }
+    }
+}
