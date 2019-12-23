16c776692ca3243850b16a43d8be07c0d8153704
Checha07 <58676214+Checha07@users.noreply.github.com>
2019-12-23 21:07:21 +0700

    Add files via upload

---
+++ практическая работа 2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _22222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+         
+            double n = Convert.ToDouble(Console.ReadLine());
+            double x = 0;
+            double a = 0;
+            for ( double i=1; i<=n; i++)
+            {
+                x = (1.0 + (1.0 / (i * i)));
+                a += x;
+            }
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+        }
+    }
+}
